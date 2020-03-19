ec36c2da8bd46e7eaa07eecb9d2d5c943d9f3232
deepa <deepa@LAPTOP-5LTLQDNP>
2020-03-19 15:04:04 +0530

    updates

--- auto.org/src/test/java/selenium/NewTest.java
+++ auto.org/src/test/java/selenium/NewTest.java
@@ -3,7 +3,7 @@ package selenium;
 import static org.testng.Assert.assertEquals;
 
 import java.io.File;
-
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -22,95 +22,104 @@ import org.testng.annotations.Test;
 public class NewTest {
 
 	WebDriver driver;
-	
+
 	@BeforeMethod
-	
+
 	public void setUp() {
 		String loc=System.getProperty("user.dir");
 		System.setProperty("webdriver.chrome.driver", loc+File.separator+"drivers"+File.separator+"chromedriver.exe");
-  //System.setProperty("webdriver.chrome.driver", "C:/Users/deepa/Downloads/chromedriver_win32 (3)/chromedriver.exe");
-  driver=new ChromeDriver();
 
-  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-  driver.manage().window().maximize();
-  driver.manage().deleteAllCookies();
-  driver.get("https://www.amazon.in/");
-  	System.out.println(driver.manage().window().getSize());
-	Dimension d = new Dimension(1024,1366);//Screen resolution
-	//Resize the current window to the given dimension
-	driver.manage().window().setSize(d);
+		driver=new ChromeDriver();
+
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.amazon.in/");
+		System.out.println(driver.manage().window().getSize());
+		Dimension d = new Dimension(1024,1366);//Screen resolution
+
+		driver.manage().window().setSize(d);//Resize the current window to the given dimension
 	}
-	
-@Test(dataProvider="SearchProvider")
 
-public void PurchaseProduct(String username,String pwd) throws InterruptedException{
-	
-  driver.findElement(By.id("nav-link-accountList")).click();
+	@Test(dataProvider="SearchProvider")
 
-  driver.findElement(By.id("ap_email")).sendKeys(username);//Enter user name
-  driver.findElement(By.id("continue")).click();
+	public void PurchaseProduct(String username,String pwd) throws InterruptedException{
 
-  driver.findElement(By.id("ap_password")).sendKeys(pwd);//Enter password
-  driver.findElement(By.id("signInSubmit")).click();
+		driver.findElement(By.id("nav-link-accountList")).click();
 
-  driver.findElement(By.id("twotabsearchtextbox")).sendKeys("65-inch TV");//search
-  driver.findElement(By.xpath("//input[@value='Go']")).click();
- 
-  driver.findElement(By.xpath("(//img[@class='s-image'])[3]")).click();//random select
+		driver.findElement(By.id("ap_email")).sendKeys(username);//Enter user name
+		driver.findElement(By.id("continue")).click();
 
-  for(String winHandle : driver.getWindowHandles()){
-      driver.switchTo().window(winHandle);
-  }
+		driver.findElement(By.id("ap_password")).sendKeys(pwd);//Enter password
+		driver.findElement(By.id("signInSubmit")).click();
 
-  String txt= driver.findElement(By.id("productTitle")).getText();
-  Thread.sleep(10000);
-  
-  	//Assert.assertTrue(True, txt.contains("65 inches"));
-    Assert.assertTrue(txt.contains("65 inches"),"TestCase-FAILED");
-    System.out.println("Tetscase-PASSED");
-    
-  /*if(txt.contains("65 inches")) {
-  	System.out.println("65 inch TV is displayed");
-  }
-  else {
-  	System.out.println("65 inch TV is not displayed");
-  }*/
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("65-inch TV");//search for product
+		driver.findElement(By.xpath("//input[@value='Go']")).click();
 
-  driver.findElement(By.id("add-to-cart-button")).click();
+		driver.findElement(By.xpath("(//img[@class='s-image'])[2]")).click();//select the product
 
-  driver.findElement(By.id("hlb-ptc-btn-native"));
-  
-  driver.findElement(By.id("nav-cart-count")).click();
-  driver.findElement(By.xpath("//input[@value='Delete']")).click();
-  
-  
-  Actions action =new Actions(driver);
-  WebElement ele=driver.findElement(By.id("nav-link-accountList"));
-  action.moveToElement(ele).perform();
-  
-  
-  Thread.sleep(5000);
-  driver.findElement(By.xpath("//a[@id='nav-item-signout']")).click();
-}
+		for(String winHandle : driver.getWindowHandles()){
+			driver.switchTo().window(winHandle);
+		}
+
+		String txt= driver.findElement(By.id("productTitle")).getText();
+		Thread.sleep(10000);
+
+
+		Assert.assertTrue(txt.contains("65 inches"),"TestCase-FAILED");
+		System.out.println("Tetscase-PASSED");
+
+		//To add product to cart and enter address details for product delivery
+		driver.findElement(By.id("add-to-cart-button")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//a[@id='hlb-ptc-btn-native']")).click();
+		driver.findElement(By.id("enterAddressFullName")).sendKeys("Sumana");
+		driver.findElement(By.id("enterAddressPhoneNumber")).sendKeys("9745344232");
+		driver.findElement(By.id("enterAddressPostalCode")).sendKeys("560097");
+		driver.findElement(By.id("enterAddressAddressLine1")).sendKeys("10");
+		driver.findElement(By.id("enterAddressAddressLine2")).sendKeys("Vidyaranyapura");
+		driver.findElement(By.id("enterAddressCity")).sendKeys("Bangalore");
+		driver.findElement(By.id("enterAddressStateOrRegion")).sendKeys("Karnataka");
+		driver.findElement(By.xpath("//input[contains(@class,'submit-button')]")).isDisplayed();
+		System.out.println("Address page is displayed");
+
+		//To delete the added product from cart
+		ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+		driver.switchTo().window(tabs.get(0));
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+
+		driver.findElement(By.id("nav-cart-count")).click();
+		driver.findElement(By.xpath("//input[@value='Delete']")).click();
+
+
+		Actions action =new Actions(driver);
+		WebElement ele=driver.findElement(By.id("nav-link-accountList"));
+		action.moveToElement(ele).perform();
+
+
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//a[@id='nav-item-signout']")).click();
+	}
 
 	@AfterMethod
-  public void CloseBrowser(){
-	  driver.quit();
-  }
-	
-	@DataProvider(name="SearchProvider")
-    public Object[][] getDataFromDataprovider(){
-		
-		Object[][] groupArray = null;
-		
-		groupArray = new Object[][]
-    	{
-            { "9710014423", "Sumana@6" }
-           
-        };
-        return groupArray;
+	public void CloseBrowser(){
+		driver.quit();
+	}
 
-}
+	@DataProvider(name="SearchProvider")
+	public Object[][] getDataFromDataprovider(){
+
+		Object[][] groupArray = null;
+
+		groupArray = new Object[][]
+				{
+			{ "9710014423", "Sumana@6" }
+
+				};
+				return groupArray;
+
+	}
 }
 
 
